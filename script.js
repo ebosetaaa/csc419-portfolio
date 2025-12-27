b5b5a5938cdc128b718efa27ed04d874aaa45ff1
ebosetaaa <93073863+ebosetaaa@users.noreply.github.com>
2025-12-27 22:28:29 +0100

    Update website content and styling

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+const navItems = document.querySelectorAll('.nav-links a');
+
+navItems.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
